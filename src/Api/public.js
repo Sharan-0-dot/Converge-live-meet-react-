9b580dc962bcca1d4ca9c544a0b7b0b42148920f
Sharan-0-dot <sharan482@gmail.com>
2025-08-25 07:19:02 +0530

    fixed rendering issue

--- src/Api/public.js
+++ src/Api/public.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
     baseURL: "https://converge-frow.onrender.com",
-    // "http://localhost:8080",
+    // "http://localhost:8080", 
 });
 
 export default axiosPublic;
