d0d4ec3af492643d656737c6342b878767fc36f3
Sharan-0-dot <sharan482@gmail.com>
2025-08-25 06:31:01 +0530

    Updated backend url

--- src/Api/public.js
+++ src/Api/public.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://converge-frow.onrender.com",
+    // "http://localhost:8080",
 });
 
 export default axiosPublic;
